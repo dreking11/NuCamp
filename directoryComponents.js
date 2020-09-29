c4da4ab8b2d542cae29f494b9d4da8b000c2d88e
dreking11 <64158658+dreking11@users.noreply.github.com>
2020-09-29 15:18:02 -0400

    commit
    Redux Thunk

--- directoryComponents.js
+++ directoryComponents.js
@@ -1,13 +1,17 @@
 import React from 'react' ;
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { Loading } from './loadingComponent';
 
-function RenderDirectoryItem({campsite, onClick}) {
+function RenderDirectoryItem({campsite}) {
     return(
         <Card>
-            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-            <CardImgOverlay>
-                <CardTitle>{campsite.name}</CardTitle>
-            </CardImgOverlay>
+            <Link to={`/directory/${campsite.id}`}>
+                <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                <CardImgOverlay>
+                    <CardTitle>{campsite.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
         </Card>
 
     );
@@ -16,7 +20,7 @@ function RenderDirectoryItem({campsite, onClick}) {
 
 function Directory(props){
     
-    const directory = props.campsites.map(campsite => {
+    const directory = props.campsites.campsites.map(campsite => {
         return (
             <div key={campsite.id} className="col-md-5 m-1">
                 <RenderDirectoryItem campsite={campsite} />
@@ -24,13 +28,44 @@ function Directory(props){
         );
     });
 
+    if(props.campsites.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        ) 
+    }
+    if (props.campsites.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.campsites.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
     return (
-        <div className="conatiner">
-            <div className="row">
-                {directory}
+        <div className="container">
+        <div className="row">
+            <div className="col">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>Directory</BreadcrumbItem>
+                </Breadcrumb>
+                <h2>Directory</h2>
+                <hr />
             </div>
         </div>
-    );
+        <div className="row">
+            {directory}
+        </div>
+    </div>
+);
+        
 }                                                                  
 
 export default Directory;
