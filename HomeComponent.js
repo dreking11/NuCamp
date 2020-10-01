57f6997711f72bc70682cf1186e142620575ebe9
dreking11 <64158658+dreking11@users.noreply.github.com>
2020-10-01 13:33:37 -0400

    commit
    Fetch From Server

--- HomeComponent.js
+++ HomeComponent.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-import { Loading } from './loadingComponent'
+import { Loading } from './loadingComponent';
+import {baseUrl} from '../shared/baseUrl';
+
 function RenderCard({item, isLoading, errMess}) {
     if (isLoading) {
         return <Loading/>;
@@ -10,7 +12,7 @@ function RenderCard({item, isLoading, errMess}) {
     }
     return (
         <Card>
-            <CardImg src={item.image} alt={item.name} />
+            <CardImg src={baseUrl + item.image} alt={item.name} />
             <CardBody>
                 <CardTitle>{item.name}</CardTitle>
                 <CardText>{item.description}</CardText>
@@ -30,7 +32,11 @@ function Home(props) {
                   /> 
                </div>
                <div className="col-md m-1">
-                  <RenderCard item={props.promotion} />  
+                  <RenderCard
+                   item={props.promotion}
+                   isLoading={props.promotionLoading}
+                   errMess={props.promotionErrMess}
+                  />  
                </div>
                <div className="col-md m-1">
                   <RenderCard item={props.partner} />  
