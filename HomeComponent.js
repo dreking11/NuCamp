aebff72c2f3b0db9e80b2fc81d38dc421a6db5dd
dreking11 <64158658+dreking11@users.noreply.github.com>
2020-10-16 10:00:42 -0400

    Commit React-Native
    Navigation Part 2

--- HomeComponent.js
+++ HomeComponent.js
@@ -1,56 +1,19 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-import { Loading } from './loadingComponent';
-import { baseUrl } from '../shared/baseUrl';
-import { FadeTransform } from 'react-animation-components';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 
-function RenderCard({item, isLoading, errMess}) {
-    if (isLoading) {
-        return <Loading/>;
+class Home extends Component {
+
+    static navigationOptions = {
+        title: 'Home'
     }
-    if(errMess) {
-    return <h4>{errMess}</h4>;
+
+    render() {
+        return (
+            <View>
+                <Text>Home Component</Text>
+            </View>
+        );
     }
-    return (
-        <FadeTransform
-            in 
-            transformProps={{
-                exitTransform: 'scale(0.5) translateY(50%)'
-            }}>
-            <Card>
-                <CardImg src={baseUrl + item.image} alt={item.name} />
-                <CardBody>
-                    <CardTitle>{item.name}</CardTitle>
-                    <CardText>{item.description}</CardText>
-                </CardBody>
-            </Card>
-        </FadeTransform>    
-    );
 }
 
-function Home(props) {
-    return (
-        <div className="container">
-           <div className="row">
-               <div className="col-md m-1">
-                  <RenderCard item={props.campsite} 
-                  isLoading={props.campsitesLoading}
-                  errMess={props.campsitesErrMess}
-                  /> 
-               </div>
-               <div className="col-md m-1">
-                  <RenderCard
-                   item={props.promotion}
-                   isLoading={props.promotionLoading}
-                   errMess={props.promotionErrMess}
-                  />  
-               </div>
-               <div className="col-md m-1">
-                  <RenderCard item={props.partner} />  
-               </div>
-           </div>
-        </div>
-    );
-}
-
-export default Home;   
+export default Home;
